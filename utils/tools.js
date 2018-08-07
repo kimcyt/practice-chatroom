3778b8660a3d0edff446101e579d2fe8f4938751
kimbowa <haohua.li@stargt.com.cn>
2018-08-07 14:49:42 +0800

    added password hashing

--- utils/tools.js
+++ utils/tools.js
@@ -5,22 +5,32 @@ function formatMessage(users, type, username, userId, data) {
     let msg = {};
     msg.userList = users || "";
     msg.type = type || "";
-    if(type==="join")
+    if(type==="join" || type==="left")
         msg.data= {userId: userId, data: username + data};
     else
         msg.data = {userId: userId, data: data};
     return msg;
 }
+//
+// function parseUrl(reqUrl){
+//     let parsedUrl = url.parse(reqUrl, {parseQueryString: true});
+//     let userId = parsedUrl["query"]["userId"] || "";
+//     let username = parsedUrl["query"]["username"] || "";
+//     let password = parsedUrl["query"]["password"] || "";
+//     return [userId, username, password];
+// }
 
-function parseUrl(reqUrl){
-    let parsedUrl = url.parse(reqUrl, {parseQueryString: true});
-    let userId = parsedUrl["query"]["userId"] || "";
-    let username = parsedUrl["query"]["username"] || "";
-    let password = parsedUrl["query"]["password"] || "";
-    return [userId, username, password];
+function convertToList(users) {
+    let userList = [];
+    for(let userId in users){
+        userList.push(users[userId]);
+    }
+    return userList;
 }
+
 
 module.exports = {
     formatMsg: formatMessage,
-    parseUser: parseUrl
+    // parseUser: parseUrl,
+    convertToList: convertToList
 };
